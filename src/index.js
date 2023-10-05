963cec248046a45ff410cce3dccfdd06c7b28b16
PapiLuee <41023873+LueDev@users.noreply.github.com>
2023-10-05 00:10:56 -0400

    Created a successful todo list without bugs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector('#create-task-form')
+  const submit = document.querySelector("#create-task-form > input[type=submit]:nth-child(3)")
+
+  submit.addEventListener('click', (e) =>{
+    e.preventDefault() //prevents the submit side-effect
+    renderToDo(e.target.parentElement.children[1].value) //grabs the textfield value
+    form.reset() //resets our form textfield.
+  })
+
+  // console.log("FORM: ", form, " \n\nSUBMIT: ", submit)
 });
+
+function renderToDo(task){
+  const listItem = document.createElement('li')
+  const btn = document.createElement('button')
+  btn.addEventListener('click', (e) => {
+    e.target.parentElement.remove() 
+  })
+  listItem.textContent = `${task} `
+  btn.textContent = "X"
+  listItem.append(btn)
+  document.querySelector('#tasks').append(listItem)
+}
